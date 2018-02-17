41f18a4bdb7cf4b45f74e2d15685258a9a00fe61
dlaitine <dlaitine@gmail.com>
2018-02-17 10:37:01 +0200

    Add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         path: __dirname + '/build',
         filename: 'bundle.js'
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         loaders: [
             {
                 exclude: /node_modules/,
             loader: 'babel-loader',
             query: {
-                presets: ['react', 'es2015']
+                presets: ['react', 'es2015'],
+                plugins: ['transform-decorators-legacy', 'transform-class-properties']
             }
         }]
     },
