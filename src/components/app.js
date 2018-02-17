76cd21eece5fb6b6ff5fe47e8a6bd53418c46605
dlaitine <dlaitine@gmail.com>
2018-02-17 15:16:00 +0200

    Add sorting

--- src/components/app.js
+++ src/components/app.js
@@ -9,6 +9,14 @@ export default class App extends Component {
     }
 
     render() {
-        return(<CountryList store={this.props.store} />);
+        return(
+        <div>
+            <button onClick={this.props.store.sortByName}>Sort by name</button>
+            <button onClick={this.props.store.sortByPopulation}>Sort by population</button>
+            <button onClick={this.props.store.sortByArea}>Sort by area</button>
+            <button onClick={this.props.store.onlyEnglishSpeaking}>Only english speaking countries</button>
+            <CountryList store={this.props.store} />
+        </div>
+        );
     }
 }
--- src/stores/CountryStore.js
+++ src/stores/CountryStore.js
@@ -2,6 +2,12 @@ import { observable, action, computed } from 'mobx';
 import axios from 'axios';
 
 export default class CountryStore {
+    constructor() {
+        this.sortByName = this.sortByName.bind(this);
+        this.sortByPopulation = this.sortByPopulation.bind(this);
+        this.sortByArea = this.sortByArea.bind(this);
+        this.onlyEnglishSpeaking = this.onlyEnglishSpeaking.bind(this);
+    }
 
     @observable countries = [];
 
@@ -15,5 +21,55 @@ export default class CountryStore {
             .catch((error) => {
                 console.log(error);
             })
+    }
+
+    @action
+    sortByName() {
+        this.countries = this.countries.sort((a, b) => {
+            if(a.name < b.name)
+                return -1;
+            else if(a.name > b.name)
+                return 1;
+            else
+                return 0;
+        });
+    }
+
+    @action
+    sortByPopulation() {
+        this.countries = this.countries.sort((a, b) => {
+            if(a.population > b.population)
+                return -1;
+            else if(a.population < b.population)
+                return 1;
+            else
+                return 0;
+        });
+    }
+
+    @action
+    sortByArea() {
+       this.countries = this.countries.sort((a, b) => {
+            if(a.area > b.area)
+                return -1;
+            else if(a.area < b.area)
+                return 1;
+            else
+                return 0;
+        });
+    }
+
+    @action
+    onlyEnglishSpeaking() {
+        this.countries = this.countries.filter(country => {
+            for(var i = 0; i < country.languages.length; i++) {
+                 if(country.languages.get(i).iso639_1 === "en") {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            } 
+        });
     }
 }
