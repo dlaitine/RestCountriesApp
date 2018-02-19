eeef6f87fb1986f9699db154a73c13bdd536b65c
dlaitine <dlaitine@gmail.com>
2018-02-19 10:45:01 +0200

    Show filter state

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { inject } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 
 @inject('store')
+@observer
 export default class Sorting extends Component {
     render() {
         return(
@@ -9,7 +10,7 @@ export default class Sorting extends Component {
                 <button onClick={this.props.store.sortByName}>Sort by name</button>
                 <button onClick={this.props.store.sortByPopulation}>Sort by population</button>
                 <button onClick={this.props.store.sortByArea}>Sort by area</button>
-                <button onClick={this.props.store.onlyEnglishSpeaking}>Only english speaking countries</button>
+                <button onClick={this.props.store.onlyEnglishSpeaking}>Only english speaking countries: {this.props.store.onlyEnglishSpeakingFilter.toString()}</button>
             </div>
         );
     }
