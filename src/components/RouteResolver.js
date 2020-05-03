4ab09d2c7bb0af0d12647c2a097199a7daf32bba
Daniel Laitinen <dlaitine@gmail.com>
2020-05-03 15:09:55 +0300

    Reset target country when reloading view

--- src/components/RouteResolver.js
+++ src/components/RouteResolver.js
@@ -15,6 +15,7 @@ export default class RouteResolver extends Component {
 
     constructor(props) {
         super(props);
+        this.props.store.setTargetCountry("");
         this.onSelectChange = this.onSelectChange.bind(this);
     }
 
